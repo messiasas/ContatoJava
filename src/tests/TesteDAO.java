41657e9952aa09afc8e4f5ae64ef6bf1fd30c701
messiasas <filipe.silva8@aluno.cps.sp.gov.br>
2026-08-16 22:05:34 -0300

    Feat: Testes completos

--- src/tests/TesteDAO.java
+++ src/tests/TesteDAO.java
@@ -1,9 +1,12 @@
 package tests;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import model.Contato;
 import dao.ContatoDAO;
+import org.w3c.dom.ls.LSOutput;
 
 import java.sql.SQLException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,15 +14,60 @@ public class TesteDAO {
     public static void main(String[] args) throws SQLException {
 
         ContatoDAO dao = new ContatoDAO();
-       /* Contato novoContato = new Contato(0,"Messias","81019289239","messias@gmail.com");
+        Contato newCont = new Contato(0,"Filipe","1192201920","filipe@gmail.com");
 
-        dao.inserir(novoContato);
-        System.out.println("Register name - "+ novoContato.getNome() + " - created.");*/
+        dao.inserir(newCont);
 
         List<Contato> contatos = dao.listarTodos();
         for(Contato c: contatos){
-            System.out.println("ID: "+c.getId()+" | Name: "+c.getNome()+" | Tel: "+c.getTelefone()+" | Email: "+c.getEmail()+".");
+            System.out.println("ID: "+c.getId()+" | Name: "+c.getNome()+" | Telefone: "+c.getTelefone()+" | Email: "+c.getEmail());
         }
+
+        Contato contId = contatos.get(0);
+        contId.setNome("Messias");
+        dao.atualizar(contId);
+
+        System.out.println();
+
+        List<Contato> contatosAtualiz = dao.listarTodos();
+
+        System.out.println("Atualizado: ");
+        for(Contato c: contatosAtualiz){
+            System.out.println("ID: "+c.getId()+" | Name: "+c.getNome()+" | Telefone: "+c.getTelefone()+" | Email: "+c.getEmail());
+        }
+
+        int delId = contId.getId();
+        dao.deletar(delId);
+
+        System.out.println();
+
+        List<Contato> contatosAt = dao.listarTodos();
+        System.out.println("Contato "+contId.getNome()+" deletado.");
+        for(Contato c: contatosAt){
+            System.out.println("ID: "+c.getId()+" | Name: "+c.getNome()+" | Telefone: "+c.getTelefone()+" | Email: "+c.getEmail());
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
+
